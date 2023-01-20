941058545c3994fbedb2b484dcfce8cd7775b8eb
Rodrigo Viana <rsviana@icloud.com>
2023-01-20 15:59:27 -0300

    Add you Key

--- location.py
+++ location.py
@@ -3,7 +3,7 @@ import socket,ipinfo,requests,os
 def get_location():
     external_ip = requests.get("https://ifconfig.me/ip").text.strip()
     print(external_ip)
-    access_token = '85986aa7a82a43'
+    access_token = 'YouKey: ipinfo.io'
     handler = ipinfo.getHandler(access_token)
     details = handler.getDetails(external_ip)
     print(f'Localização do host: {details.city}, {details.region}, {details.country}')
