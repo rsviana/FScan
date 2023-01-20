95a0b228b05c3ad9792432e0bc1ee47d5fb17292
Rodrigo Viana <rsviana@icloud.com>
2023-01-20 14:21:11 -0300

    Corrigido Bug na funcao nmap

--- scan_nmap.py
+++ scan_nmap.py
@@ -1,12 +1,18 @@
-import scan_nmap
-# em desenvolvimento....
-
+import nmap,os
 
 def scan_port(ip, port):
-    nm = scan_nmap.PortScanner()
-    nm.scan(str(ip), str(port))
-    if nm[ip].has_tcp(port):
-        state = nm[ip]['tcp'][port]['state']
-        print(f'Port {port} on {ip} is {state}')
-    else:
-        print(f'Port {port} on {ip} is closed or filtered')
+    nm = nmap.PortScanner()
+    try:
+        nm.scan(str(ip), str(port))
+        if nm[ip].has_tcp(port):
+            state = nm[ip]['tcp'][port]['state']
+            print(f'Port {port} on {ip} is {state}')
+        else:
+            print(f'Port {port} on {ip} is closed or filtered')
+    except KeyError:
+        print(f'Host {ip} not found')
+
+
+    print()
+    input("Press any key to return menu")
+    os.system('python main.py') 
